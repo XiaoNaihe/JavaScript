eed44dbfa1113da410dabfc07c9c2455f79edcfc
XiaoNaihe <GabrielSSK@outlook.com>
2021-05-06 08:34:46 -0300

    06/05/2021
    Atualizando as atividades feitas até agora, mais um desafio proposto no ex014.

---
+++ CursoJS/Aula12/ex011.js
@@ -0,0 +1,9 @@
+var idade = 70
+console.log(`você tem ${idade} anos.`)
+if (idade < 18){
+    console.log('vota')
+}else if (idade < 18 || idade >65){
+    console.log('voto opcional')
+}else{
+    console.log('voto obrigatorio')
+}
---
+++ CursoJS/Aula12/ex013.js
@@ -0,0 +1,39 @@
+var agora = new Date()
+var diasem = agora.getDay()
+/*
+    Domingo
+    Segunda
+    Terça
+    Quarta
+    Quinta
+    Sexta
+    Sabado
+*/
+
+//console.log(diasem)
+switch(diasem){
+    case 0:
+        console.log('Domingo')
+        break
+    case 1:
+        console.log('Segunda Feira')
+        break
+    case 2:
+        console.log('Terça Feira')
+        break
+    case 3:
+        console.log('Quarta Feira')
+        break
+    case 4:
+        console.log('Quinta Feira')
+        break
+    case 5:
+        console.log('Sexta Feira')
+        break
+    case 6:
+        console.log('Sabado')
+        break
+    default:
+        console.log('[ERRO] Dia invalido!')
+        break
+}
