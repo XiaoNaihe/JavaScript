2dcee2b3c97e68218ff8cc24ae7345d72e1f1c34
XiaoNaihe <GabrielSSK@outlook.com>
2021-06-06 15:12:55 -0300

    atualização atividade
    06/06/21

---
+++ CursoJS/Aula13/ambiente.js
@@ -0,0 +1,18 @@
+//Do while (repetição no final)
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+}
+while (c <= 10 )
+
+
+
+// while (repetição no inicio)
+/*
+var c = 1
+while (c <= 10 ){
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
